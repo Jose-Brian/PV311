9d1e141d0feb8f5d4efa1bd651f38775feb87491
Brian Nochebuena <108840189+Jose-Brian@users.noreply.github.com>
2024-04-08 10:24:04 -0600

    Add files via upload

---
+++ PV31/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using PV31.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PV31
+{
+    public class DataContext:DbContext
+
+    {
+        public DbSet<Seller> Sellers { get; set; } 
+        public DataContext():base("namecon") { }
+    }
+}
